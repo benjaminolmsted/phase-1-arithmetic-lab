518d26db5ffff7c30275dff35ad41a97daf3fa83
benjamin <benjamin.olmsted@gmail.com>
2021-06-27 09:43:06 -0700

    commit tree be751ba4a3db48041fea8a404579c5f460a3a5b0

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*5) + 1;
+
+const num3 = 14;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(20, 1, 3, 14, 15, 17);
